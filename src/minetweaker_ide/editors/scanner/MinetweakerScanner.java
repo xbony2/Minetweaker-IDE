a0a49c8fdf003fd1bd7622de8b9e92a9ffbdf353
xbony2 <xbony22@gmail.com>
2014-11-17 17:43:41 -0500

    Probably not going to work?

--- src/minetweaker_ide/editors/scanner/MinetweakerScanner.java
+++ src/minetweaker_ide/editors/scanner/MinetweakerScanner.java
@@ -11,11 +11,13 @@ public class MinetweakerScanner extends RuleBasedScanner {
 
 	public MinetweakerScanner(ColorManager manager) {
 		IToken procInstr = new Token(new TextAttribute(manager.getColor(IMinetweakerColorConstants.ZS_COMMENT)));
+		IToken keyword = new Token(new TextAttribute(manager.getColor(IMinetweakerColorConstants.KEYWORD)));
 
-		IRule[] rules = new IRule[3];
+		IRule[] rules = new IRule[4];
 		rules[0] = new EndOfLineRule("#", procInstr);
 		rules[1] = new EndOfLineRule("//", procInstr);
-		rules[2] = new WhitespaceRule(new MinetweakerWhitespaceDetector());
+		rules[2] = new WordRule(null, keyword);
+		rules[3] = new WhitespaceRule(new MinetweakerWhitespaceDetector());
 
 		setRules(rules);
 	}
